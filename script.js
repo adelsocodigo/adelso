dd038cd00598e28d545a5ea191256ba1609c6a2c
adelsocodigo <adsel85@gmail.com>
2024-07-09 17:36:32 -0500

    Primer Commit

---
+++ script.js
@@ -0,0 +1,161 @@
+function cambiarAdministracion() {
+    document.getElementById("miImagen").src = "assets/img/administracion.png";
+    document.getElementById("miImagen").alt = "Imagen de programación";
+    document.getElementById("miParrafo").innerHTML = `
+        <h2>Administración</h2>
+        <p>Como licenciado en Educación para el Trabajo sub. área comercial, poseo una sólida
+        formación y experiencia que me capacitan para desempeñarme eficazmente en roles administrativos, comerciales,
+        gerenciales y de supervisión. Mi trayectoria profesional me ha permitido desarrollar habilidades clave en la
+        gestión de equipos, la planificación estratégica y la optimización de procesos. Además, tengo una destacada 
+        capacidad para la resolución de problemas y la toma de decisiones, así como una excelente aptitud 
+        para la comunicación. Mi enfoque está orientado a alcanzar objetivos organizacionales, 
+        mejorar la eficiencia operativa y fomentar un entorno de trabajo colaborativo y productivo. 
+        
+        Estoy comprometido con la mejora continua y dispuesto a aportar mis conocimientos y competencias para 
+        contribuir al éxito y crecimiento de la organización empresarial.</p>
+        <section>
+            <h2>Mis Habilidades</h2>
+            <ul>
+                <li><strong>Gestión de Equipos:</strong> Capacidad para liderar, motivar y desarrollar equipos 
+                de trabajo de alto rendimiento.</li>
+                <li><strong>Planificación Estratégica:</strong> Habilidad para diseñar y ejecutar planes 
+                estratégicos alineados con los objetivos organizacionales.</li>
+                <li><strong>Optimización de Procesos:</strong> Competencia en identificar y mejorar procesos 
+                operativos para aumentar la eficiencia y reducir costos.</li>
+                <li><strong>Resolución de Problemas:</strong> Capacidad para analizar situaciones complejas y 
+                encontrar soluciones efectivas y rápidas.</li>
+                <li><strong>Toma de Decisiones:</strong> Habilidad para tomar decisiones informadas basadas
+                en datos y análisis detallados.</li>
+                <li><strong>Comunicación Efectiva:</strong> Excelentes habilidades de comunicación oral y 
+                escrita para interactuar con diferentes niveles de la organización.</li>
+                <li><strong>Orientación a Resultados:</strong> Enfoque constante en alcanzar y superar los
+                 objetivos y metas establecidos.</li>
+                <li><strong>Nivel de Inglés:</strong> A2. Actualmente, me estoy enfocando en mejorarlo, consciente 
+                de la importancia y las exigencias del mercado laboral en cuanto a tener un nivel de inglés fluido.</li>
+
+            </ul>
+        </section>
+        <section>
+            <h2>Experiencia</h2>
+            <p>He tenido la gran oportunidad de desempeñarme en diversas empresas y poner en práctica los 
+            conocimientos antes mencionados</p>
+            <li><strong>Tecnologia Educativa de Carabobo (Venezuela):</strong> En esta empresa me destaqué en el 
+            diseño de material intruccional docente; es decir en la creación y diseño de manuales educativos que
+            los docentes utilizarian para impartir las clases de computación, adicionalmente cumplí con labores de 
+            supervisión y manejo de personal docente para que los mismos cumplieran cabalmente con las directrices 
+            y los manuales educativos.</li>
+            <li><strong>Síragom (Venezuela):</strong> La única empresa ensambladora de hardware informático del país;
+             aquí tuve la maravillosa oportunidad de participar en todas las áreas desde el ensamblaje de computadoras, 
+             servidores y cámaras digitales hasta el proceso de control de calidad en la cual se hacía una revisión 
+             exhaustiva del hardware y del sistema operativo, lo que me llevó a obtener un aprendizaje significativo en
+              todo el proceso de producción.</li>
+        </section>
+        <section>
+            <h2>¿Por qué Contratarme?</h2>
+            <p>Mi enfoque autodidacta no solo demuestra mi capacidad de aprendizaje continuo, sino también mi
+            habilidad para adaptarme rápidamente a nuevas tecnologías y resolver problemas de manera
+            efectiva. Soy un candidato ideal para roles que requieran creatividad, iniciativa y una fuerte
+            ética de trabajo.</p>
+        </section>
+    `;
+}
+
+
+
+function cambiarInformatica() {
+    document.getElementById("miImagen").src = "assets/img/programacion.jpg";
+    document.getElementById("miImagen").alt = "Imagen de programación";
+    document.getElementById("miParrafo").innerHTML = `
+      <h2>Informática</h2>
+      <p>Soy un profesional autodidacta con habilidades avanzadas en diversas áreas tecnológicas y
+      administrativas. Mi experiencia abarca desde la creación y gestión de sitios web utilizando HTML,
+      CSS, JavaScript y GitHub. hasta el manejo experto de herramientas como Word y Excel.</p>
+      <section>
+          <h2>Mis Habilidades</h2>
+          <ul>
+              <li><strong>Desarrollo Web:</strong> Conocimientos en HTML, CSS y JavaScript, creando sitios web dinámicos
+              y atráctivos.</li>
+              <li><strong>Autodidactismo:</strong> Tengo una pasión por aprender y mejorar constantemente mis
+              habilidades tecnológicas.</li>
+              <li><strong>Gestión de Datos:</strong> Manejo avanzado de Microsoft Word y Excel para análisis y
+              presentación de datos.</li>
+              <li><strong>Colaboración:</strong> Uso básico pero efectivo de GitHub para control de versiones
+              y colaboración en proyectos.</li>
+              <li><strong>Nivel de Inglés:</strong> A2. Actualmente, me estoy enfocando en mejorarlo, consciente 
+                de la importancia y las exigencias del mercado laboral en cuanto a tener un nivel de inglés fluido.</li>
+          </ul>
+      </section>
+        <section>
+            <h2>Experiencia</h2>
+            <p>He tenido la gran oportunidad de desempeñarme en diversas empresas y poner en práctica los 
+            conocimientos antes mencionados</p>
+            <li><strong>Tecnologia Educativa de Carabobo (Venezuela):</strong> En esta empresa me destaqué en el 
+            diseño de material intruccional docente; es decir en la creación y diseño de manuales educativos que
+            los docentes utilizarian para impartir las clases de computación, adicionalmente cumplí con labores de 
+            supervisión y manejo de personal docente para que los mismos cumplieran cabalmente con las directrices 
+            y los manuales educativos.</li>
+            <li><strong>Síragom (Venezuela):</strong> La única empresa ensambladora de hardware informático del país;
+             aquí tuve la maravillosa oportunidad de participar en todas las áreas desde el ensamblaje de computadoras, 
+             servidores y cámaras digitales hasta el proceso de control de calidad en la cual se hacía una revisión 
+             exhaustiva del hardware y del sistema operativo, lo que me llevó a obtener un aprendizaje significativo en
+              todo el proceso de producción.</li>
+        </section>
+
+
+
+      <section>
+          <h2>Proyectos Destacados</h2>
+          <p>He creado exitosamente una página web para una radio online <a href="http://radiogotica.com" target="_blank">radiogotica.com</a>, demostrando mi capacidad para llevar
+          ideas creativas a la realidad digital.</p>
+      </section>
+      <section>
+          <h2>¿Por qué Contratarme?</h2>
+          <p>Mi enfoque autodidacta no solo demuestra mi capacidad de aprendizaje continuo, sino también mi
+          habilidad para adaptarme rápidamente a nuevas tecnologías y resolver problemas de manera
+          efectiva. Soy un candidato ideal para roles que requieran creatividad, iniciativa y una fuerte
+          ética de trabajo.</p>
+      </section>
+  `;
+}
+
+function cambiarElectricidad() {
+    document.getElementById("miImagen").src = "assets/img/electricista.jpg";
+    document.getElementById("miImagen").alt = "Imagen de electricista";
+    document.getElementById("miParrafo").innerHTML = `
+      <h2>Electricista de Mantenimiento Industrial</h2>
+      <p>Soy un profesional con amplios conocimientos y experiencia en el ramo eléctrico, tanto residencial como industrial.</p>
+      <section>
+          <h2>Mis Habilidades</h2>
+          <ul>
+              <li><strong>Sistemas de Control:</strong> Una de mis mayores virtudes es el dominio de sistemas 
+              de control; Reles, Contactores, Temporizadores.</li>
+              <li><strong>PLC:</strong>Uso y manejo de PLC Logo Comfort. El mundo del PLC (Program logic control), 
+              es como el de la programación nunca descansa en sus procesos de actualización, pero gracias a mi 
+              constante aprendizaje me adapto a cada uno de los cambios del entorno.</li>
+              <li><strong>Gestión de Datos:</strong> Manejo avanzado de Microsoft Word y Excel para análisis y 
+              presentación de datos.</li>
+              <li><strong>Nivel de Inglés:</strong> A2. Actualmente, me estoy enfocando en mejorarlo, consciente 
+                de la importancia y las exigencias del mercado laboral en cuanto a tener un nivel de inglés fluido.</li>
+          </ul>
+      </section>
+      <section>
+          <h2>Experiencia Laboral y Proyectos Destacados</h2>
+          <p>He participado en la empresa TEMI (Tableros eléctricos de Manufactura Industrial) en la fabricación de 
+          tableros eléctricos para los sistemas de Metro de Caracas (Venezuela), en diversos equipos pertenecientes a 
+          la cadena de produción de: Ensamblaje Metalmecánico, Cableados de Control, Sistemas de Potencia y Control de 
+          Calidad.</p>
+          
+      </section>
+      <section>
+          <h2>¿Por qué Contratarme?</h2>
+          <p>Mi enfoque autodidacta no solo demuestra mi capacidad de aprendizaje continuo, sino también mi habilidad 
+          para adaptarme rápidamente a nuevas tecnologías y resolver problemas de manera efectiva. Soy un candidato 
+          ideal para roles que requieran creatividad, iniciativa y una fuerte ética de trabajo.</p>
+      </section>
+  `;
+}
+
+
+
+
+
